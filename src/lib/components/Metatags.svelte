03e7854276193a5baec54728a99f485d7259985a
Sam Cheng <51756184+Samalander0@users.noreply.github.com>
2024-07-16 21:41:13 -0700

    updated metatags

--- src/lib/components/Metatags.svelte
+++ src/lib/components/Metatags.svelte
@@ -2,19 +2,19 @@
   <!-- Primary Meta Tags -->
   <title>Tome</title>
   <meta name="title" content="Tome" />
-  <meta name="description" content="Searchable curated design inspiration. More coming soon." />
+  <meta name="description" content="Searchable hand-picked design inspiration." />
 
   <!-- Open Graph / Facebook -->
   <meta property="og:type" content="website" />
   <meta property="og:url" content="https://www.tome.design/" />
   <meta property="og:title" content="Tome" />
-  <meta property="og:description" content="Searchable curated design inspiration. More coming soon." />
+  <meta property="og:description" content="Searchable hand-picked design inspiration." />
   <meta property="og:image" content="https://www.tome.design/tome-card.png" />
 
   <!-- Twitter -->
   <meta property="twitter:card" content="summary_large_image" />
   <meta property="twitter:url" content="https://www.tome.design/" />
   <meta property="twitter:title" content="Tome" />
-  <meta property="twitter:description" content="Searchable curated design inspiration. More coming soon." />
+  <meta property="twitter:description" content="Searchable hand-picked design inspiration." />
   <meta property="twitter:image" content="https://www.tome.design/tome-card.png" />
 </svelte:head>
